a5417c88752dbba80bf6033b6f28708b17f3715e
sia <mac.shacker@gmail.com>
2025-01-03 00:01:13 +0000

    add commandline params

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Main file for running the training."""
+import argparse
 
 import orbax.checkpoint as ocp
 from flax import nnx
@@ -6,21 +7,34 @@ import optax
 from train import train, get_datasets, load_checkpoint
 from modules import UNet
 
-BATCH_SIZE = 64
 
-# Enable checkpointing:
-checkpoint_dir = '/tmp/checkpoints'
-options = ocp.CheckpointManagerOptions(max_to_keep=2, create=True)
-checkpoint_manager = ocp.CheckpointManager(checkpoint_dir, options=options)
 
-epoch = 0
+def main(batcgh_size:int, checkpoint_dir:str):
+    """Main function for training the model."""
 
-if checkpoint_manager.latest_step() is not None:
-    model, epoch = load_checkpoint(checkpoint_manager, UNet(out_features=32, rngs=nnx.Rngs(0), num_channels=1))
-    print(f'Loaded from the epoch: {epoch}')
+    # Enable checkpointing:
+    options = ocp.CheckpointManagerOptions(max_to_keep=2, create=True)
+    checkpoint_manager = ocp.CheckpointManager(checkpoint_dir, options=options)
 
-# Initiate Training
-train_ds = get_datasets(BATCH_SIZE)
-model = UNet(out_features=32, rngs=nnx.Rngs(0), num_channels=1)
-optimizer = nnx.Optimizer(model, optax.adam(1e-4))
-trained_state = train(train_ds, model, optimizer, checkpoint_manager, epoch)
+    epoch = 0
+
+    if checkpoint_manager.latest_step() is not None:
+        model, epoch = load_checkpoint(
+            checkpoint_manager,
+            UNet(out_features=32, rngs=nnx.Rngs(0), num_channels=1)
+        )
+        print(f'Loaded from the epoch: {epoch}')
+
+    # Initiate Training
+    train_ds = get_datasets(batcgh_size)
+    model = UNet(out_features=32, rngs=nnx.Rngs(0), num_channels=1)
+    optimizer = nnx.Optimizer(model, optax.adam(1e-4))
+    train(train_ds, model, optimizer, checkpoint_manager, epoch)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--checkpoint_dir', type=str, default='/tmp/checkpoints')
+    args = parser.parse_args()
+    main(args.batch_size, args.checkpoint_dir)
